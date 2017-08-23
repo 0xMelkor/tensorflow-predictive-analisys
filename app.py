736e4a2663b3bcd82112f889a744dd5f5df0bca2
Andrea Simeoni <a.simeoni@reply.it>
2017-08-23 13:03:39 +0200

    batch norm

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import tensorflow as tf
+
+import os
+
+import matplotlib.pyplot as plt
 import numpy as np
 import tflearn
 from tflearn.data_utils import load_csv
 
 from data_loader.preprocess import DataPreprocessor
+
+del os.environ['TCL_LIBRARY']
 
 
 class RUL:
@@ -21,32 +26,62 @@ class RUL:
         Loads training set by index. There are 4 training documents
         :param dataset_index: [0-3] index of the document to load
         :param skip_columns: list of columns to be removed from the dataset
-        :return: for the selected training set returns 'data' (even skipped) and 'labels' 
+        :return: training a tens data and labels
         """
         if skip_columns is None:
             skip_columns = []
+
         training_file, test_file = self.data_proc.get_files_at_index(dataset_index)
-        # Load CSV file, indicate that the last column represents labels
-        # target_column indicates the labels column
-        data, labels = load_csv(training_file, categorical_labels=False)
+
+        train_x, train_y = load_csv(training_file, categorical_labels=False)
+        test_x, test_y = load_csv(test_file, categorical_labels=False)
 
         if skip_columns:
             for col in skip_columns:
-                RUL._skip_column(data, col)
+                RUL._skip_column(train_x, col)
+                RUL._skip_column(test_x, col)
 
-        np_data = np.array(data, dtype=np.double)
-        np_labels = np.array(labels, dtype=np.int32)
-        np_labels = np_labels[:, np.newaxis] # add one axis
+        np_train_x = np.array(train_x, dtype=np.float32)
+        np_train_y = np.array(train_y, dtype=np.float32)
+        np_train_y = np_train_y[:, np.newaxis]  # add one axis
 
-        return np_data, np_labels
+        np_test_x = np.array(test_x, dtype=np.float32)
+        np_test_y = np.array(test_y, dtype=np.float32)
+        np_test_y = np_test_y[:, np.newaxis]  # add one axis
+
+        return RUL.normalize_columns(np_train_x),\
+               RUL.normalize_columns(np_train_y),\
+               RUL.normalize_columns(np_test_x), \
+               RUL.normalize_columns(np_test_y)
+
+    @staticmethod
+    def normalize_columns(input_array):
+        return input_array / input_array.max(axis=0)
+
+    @staticmethod
+    def show_plot(predicted_data, real_data, num_points):
+
+        if num_points:
+            predicted_data = predicted_data[0:num_points]
+            real_data = real_data[0:num_points]
+
+        x_axis = np.arange(0, len(predicted_data), 1)
+        plt.plot(x_axis, predicted_data, marker='o', linestyle='--', label='predicted')
+        plt.plot(x_axis, real_data, marker='x', label='real data')
+        plt.show()
 
     @staticmethod
     def build_neural_network(input_len):
+        """
+        :param input_len: Number of columns for each row of data
+        :return: 
+        """
         net = tflearn.input_data(shape=[None, input_len])
-        net = tflearn.fully_connected(incoming=net, n_units=32, activation='tanh')
-        net = tflearn.fully_connected(incoming=net, n_units=32, activation='tanh')
-        net = tflearn.fully_connected(incoming=net, n_units=1, activation='tanh')
-        net = tflearn.regression(net)
+        net = tflearn.fully_connected(incoming=net, n_units=input_len, activation='sigmoid')
+        net = tflearn.batch_normalization(net)
+        net = tflearn.fully_connected(incoming=net, n_units=1, activation='sigmoid')
+        net = tflearn.regression(net, optimizer='adam', loss='mean_square',
+                                 metric='R2', learning_rate=0.001)
 
         return net
 
@@ -62,19 +97,21 @@ class RUL:
 
 
 # params
-batch_size = 64
-n_epoch = 100
+batch_size = 128
+n_epoch = 50
 
 rul = RUL(data_processor=DataPreprocessor())
 rul.preprocess_data()
-data, labels = rul.load_data(dataset_index=0, skip_columns=[0])
+data_train_x, data_train_y, test_x, test_y = rul.load_data(dataset_index=0, skip_columns=[0])
 
 # Define model
-net = RUL.build_neural_network(data.shape[1])
+net = RUL.build_neural_network(data_train_x.shape[1])
 model = tflearn.DNN(net)
 
-# add one axis
-labels = labels[:, np.newaxis]
-
 # Start training (apply gradient descent algorithm)
-model.fit(data, labels, n_epoch=n_epoch, batch_size=batch_size, show_metric=True)
+model.fit(data_train_x, data_train_y, n_epoch=n_epoch, batch_size=batch_size, show_metric=True)
+print("Optimization finished!")
+
+prediction = model.predict(data_train_x)
+
+RUL.show_plot(predicted_data=prediction, real_data=data_train_y, num_points=2000)
